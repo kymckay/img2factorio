cdbeb888a39be72eb1d560dafc329133c7a61669
SilentSpike <SilentSpike100+Github@gmail.com>
2017-08-10 11:17:36 +0100

    Improve path handling
    - Fix usage on images in subdirectories
    - Remove support for processing multiple images at once

--- img2factorio.py
+++ img2factorio.py
@@ -116,7 +116,7 @@ def black_and_white(img, threshold=128):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('image', nargs="+", help="relative path to an image file to be converted")
+    parser.add_argument('image', help="relative path to an image file to be converted")
     parser.add_argument('-p', '--preview', action="store_true", help="save a preview image and exit")
     parser.add_argument('-i', '--invert', action="store_true", help="invert the image colours")
     parser.add_argument('-t', '--tile', action="store_true", help="make the image seamless")
@@ -125,84 +125,87 @@ def main():
     parser.add_argument('--height', type=int, help="set the image height in pixels", metavar="<pixels>")
     parser.add_argument('--border', type=int, help="add a border around all edges of the image", metavar="<thickness>")
     parser.add_argument('--quantize', type=int, choices=range(1,256), help="quantize the image colours into a limited number", metavar="<number>")
-    parser.add_argument('--threshold', type=int, choices=range(0,255), default=128, help="alter the greyscale value threshold pixels are compared to",  metavar="<value>")
+    parser.add_argument('--threshold', type=int, choices=range(1,255), default=128, help="alter the greyscale value threshold pixels are compared to",  metavar="<value>")
     args = parser.parse_args()
 
-    # Attempt to process all image arguments supplied
-    for image in args.image:
-        if not os.path.isfile(image):
-            print("File doesn't exist: {}".format(image))
-        else:
-            # Open the image file
-            try:
-                img = Image.open(image)
-            except IOError:
-                print("Failed to open: {}".format(image))
-                continue
+    # Quick sanity check
+    if not os.path.isfile(args.image):
+        print("File doesn't exist:", args.image)
+        return
 
-            print("Processing: {}".format(image))
+    # Open the image file
+    try:
+        img = Image.open(args.image)
+    except IOError:
+        print("Failed to open:", args.image)
+        return
+    print("Processing:", args.image)
 
-            # Read the width and height
-            width, height = img.size
-            print("Size: {}x{}px".format(width,height))
+    # Read the width and height
+    width, height = img.size
+    print("Size: {}x{}px".format(width,height))
 
-            # Resize if specified
-            if args.scale or args.width or args.height:
-                if args.scale:
-                    width = round(width * args.scale)
-                    height = round(height * args.scale)
+    # Resize if specified
+    if args.scale or args.width or args.height:
+        if args.scale:
+            width = round(width * args.scale)
+            height = round(height * args.scale)
 
-                # Specific dimensions override scaling
-                width = args.width if args.width else width
-                height = args.height if args.height else height
+        # Specific dimensions override scaling
+        width = args.width if args.width else width
+        height = args.height if args.height else height
 
-                print("Resizing...")
-                img = img.resize((width, height), Image.BICUBIC)
-                print("New size: {}x{}px".format(width,height))
+        print("Resizing...")
+        img = img.resize((width, height), Image.BICUBIC)
+        print("New size: {}x{}px".format(width,height))
 
-            if args.invert:
-                print("Inverting...")
-                img = ImageOps.invert(img)
+    if args.invert:
+        print("Inverting...")
+        img = ImageOps.invert(img)
 
-            if args.quantize:
-                print("Quantizing...")
-                img = img.quantize(args.quantize)
+    if args.quantize:
+        print("Quantizing...")
+        img = img.quantize(args.quantize)
 
-            if args.tile:
-                print("Tiling...")
-                img = maketilable(img)
+    if args.tile:
+        print("Tiling...")
+        img = maketilable(img)
 
-            if args.border:
-                print("Adding border...")
-                img = ImageOps.expand(img, args.border)
+    if args.border:
+        print("Adding border...")
+        img = ImageOps.expand(img, args.border)
 
-            if args.preview:
-                os.makedirs("preview", exist_ok=True)
-                img.convert("1", dither=Image.NONE).save(os.path.join("preview",image))
-                print("Saved Preview")
-                continue # Preview mode doesnt write to lua
+    if args.preview:
+        os.makedirs("preview", exist_ok=True)
+        img.convert("1", dither=Image.NONE).save(os.path.join("preview", os.path.basename(args.image)))
+        print("Saved Preview")
+        return # Preview mode doesnt write to lua
 
-            # Process the image pixels
-            tiles = black_and_white(img, args.threshold)
+    # Process the image pixels
+    tiles = black_and_white(img, args.threshold)
 
-            # Prepare to output
-            scenario = os.path.join(os.getenv("APPDATA"),"Factorio\scenarios",os.path.splitext(image)[0])
-            control_lua = os.path.join(scenario,"control.lua")
-            os.makedirs(scenario, exist_ok=True)
+    # Prepare to output
+    scenario = os.path.join(os.getenv("APPDATA"), "Factorio\scenarios", os.path.basename(os.path.splitext(args.image)[0]))
+    control_lua = os.path.join(scenario, "control.lua")
+    os.makedirs(scenario, exist_ok=True)
 
-            # Copy the template file to the scenario folder
-            try:
-                copyfile("template\simple-tile.lua", control_lua)
-            except IOError:
-                print("Template failed to copy: {}".format(control_lua))
-                continue
+    # Copy the template file to the scenario folder
+    try:
+        copyfile("template\simple-tile.lua", control_lua)
+    except IOError:
+        print("Template failed to copy: {}".format(control_lua))
+        return
 
-            # Output to .lua in the respective folder
-            print("Writing...")
-            with open(control_lua, "a") as file:
-                file.write("\nwidth = {}\nheight = {}\nimg_table = ".format(width, height))
-                file.write(dict_to_lua(tiles))
-            print("Complete")
+    # Convert the tiles diectionary into a lua table string
+    print("Converting tiles...")
+    tiles = dict_to_lua(tiles)
+
+    # Output to .lua in the respective folder
+    print("Writing...")
+    with open(control_lua, "a") as file:
+        file.write("\nwidth = {}\nheight = {}\nimg_table = ".format(width, height))
+        file.write(tiles)
+    print("Complete")
 
 if __name__ == "__main__":
     main()
